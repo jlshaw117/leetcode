10efe6bd0ca8d45e51e225d22a75166ae429bfc9
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-01 15:06:37 -0800

    Add an optimized solution

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -1,8 +1,25 @@
+// const twoSum = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) return [i,j];
+//         }        
+//     }
+//     return null;
+// };
+
 const twoSum = function (nums, target) {
+    const hash = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) return [i,j];
-        }        
+        hash[nums[i]] = i;
+    }
+
+    for (let idx = 0; idx < nums.length; idx++) {
+        let curr = nums[idx];
+        let ans = target - curr;
+
+        if (curr === ans && hash[ans] !== idx) return [idx, hash[ans]];
+
+        if (hash[ans] !== undefined && hash[ans] !== idx) return [hash[curr], hash[ans]];
     }
     return null;
 };
