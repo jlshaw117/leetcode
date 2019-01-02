10cea1f2cb71a294697116e7eed80fc2708789ab
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-01 16:31:37 -0800

    Add an optimized solution useing Fibonacci

--- easy/climbStairs.js
+++ easy/climbStairs.js
@@ -1,7 +1,23 @@
-var climbStairs = function (n) {
-    if(n <= 1) return 1;
+// Brute Force recursion
 
-    return climbStairs(n-1) + climbStairs(n-2);
+// var climbStairs = function (n) {
+//     if(n <= 1) return 1;
+
+//     return climbStairs(n-1) + climbStairs(n-2);
+// };
+
+// Using Fibonacci
+
+var climbStairs = function (n) {
+    if (n < 3) return n;
+    let fib = [1, 2];
+    let ans;
+    while (fib.length < n) {
+        let i = fib.length;
+        ans = fib[i - 1] + fib[i - 2];
+        fib[i] = ans;
+    }
+    return ans;
 };
 
 
