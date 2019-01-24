385a5998a83a990357acbcd032378616d25541c4
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-24 15:50:47 -0800

    Add optimized solution

--- medium/longest_substring_without_repeats.js
+++ medium/longest_substring_without_repeats.js
@@ -1,19 +1,43 @@
+// Brute Force
+
+// const lengthOfSub = (str) => {
+//     let ans = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         sub = str[i];
+//         for (let j = i + 1; j < str.length; j++) {
+//             if (sub.includes(str[j])) {
+//                 break;
+//             } else {
+//                 sub += str[j];
+//             }
+//         }
+//         if (sub.length > ans) {
+//             ans = sub.length;
+//         }
+//     }
+//     return ans;
+// };
+
+// Optimized
+
 const lengthOfSub = (str) => {
+    let subStr = new Set();
+
     let ans = 0;
-    for (let i = 0; i < str.length; i++) {
-        sub = str[i];
-        for (let j = i + 1; j < str.length; j++) {
-            if (sub.includes(str[j])) {
-                break;
-            } else {
-                sub += str[j];
+    let i = 0;
+    let j = 0;
+
+    while (j < str.length) {
+        if (subStr.has(str[j])) {
+            subStr.delete(str[i]);
+            i ++;
+        } else {
+            subStr.add(str[j]);
+            j++;
+            if (subStr.size > ans) {
+                ans = subStr.size;
             }
-        }
-        if (sub.length > ans) {
-            ans = sub.length;
         }
     }
     return ans;
 };
-
-lengthOfSub('abcabcbb');
