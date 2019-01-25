1b356f521f7f21975ba7cc503c64c36c820d3749
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-24 18:00:59 -0800

    Add optimized solution

--- medium/zig_zag.js
+++ medium/zig_zag.js
@@ -1,36 +1,68 @@
 // Brute Force
 
+// const zigZag = (str, numRows) => {
+//     let matrix = [];
+//     for (let row = 0; row < numRows; row++) {
+//         matrix[row] = [];
+//     }
+
+//     let count = 1;
+//     let column = 0; 
+//     for (let letter = 0; letter < str.length; letter ++) {
+//         for (let row = 0; row < numRows; row++) {
+//             matrix[row][column] = str[letter];
+//             letter ++;
+//         }
+//         for (let row = numRows - 2; row  >= 0; row--) {
+//             column ++;
+//             matrix[row][column] = str[letter];
+//             letter ++;
+//         }
+//         letter -= 2;
+//     }
+
+//     let ans = '';
+//     for (let row = 0; row < matrix.length; row++) {
+//         let arr = matrix[row];
+//         for (let col = 0; col < arr.length; col++) {
+//             if (arr[col]) {
+//                 ans += arr[col];
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+// Optimized solution
+
 const zigZag = (str, numRows) => {
-    let matrix = [];
-    for (let row = 0; row < numRows; row++) {
-        matrix[row] = [];
+    if (numRows <= 1) return str;
+    let rows = [];
+    for (let i = 0; i < numRows; i++) {
+        rows[i] = '';
     }
 
-    let count = 1;
-    let column = 0; 
-    for (let letter = 0; letter < str.length; letter ++) {
-        for (let row = 0; row < numRows; row++) {
-            matrix[row][column] = str[letter];
-            letter ++;
-        }
-        for (let row = numRows - 2; row  >= 0; row--) {
-            column ++;
-            matrix[row][column] = str[letter];
-            letter ++;
-        }
-        letter -= 2;
-    }
+    let down = false;
+    let row = 0;
+    for (let letter = 0; letter < str.length; letter++) {
+        rows[row] += str[letter];
 
+        if (row % (numRows - 1) === 0) {
+            down = !down;
+        }
+
+        if (down) {
+            row ++;
+        } else {
+            row --;
+        }
+    }
     let ans = '';
-    for (let row = 0; row < matrix.length; row++) {
-        let arr = matrix[row];
-        for (let col = 0; col < arr.length; col++) {
-            if (arr[col]) {
-                ans += arr[col];
-            }
-        }
-    }
-    return ans;
-}
 
-console.log(zigZag('PAYPALISHIRING', 4));
+    rows.forEach((row) => {
+        ans += row;
+    });
+    return ans;
+};
+
+console.log(zigZag('AB', 1));
