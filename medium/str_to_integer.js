415dc529e1655f0c5e4eb2b535edda25f4694d8d
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-25 10:54:29 -0800

    Add solution for str to int

---
+++ medium/str_to_integer.js
@@ -0,0 +1,51 @@
+const strToInt = (str) => {
+    let digits = str.trim();
+    let startingValues = '+-0123456789';
+    if (!startingValues.includes(digits[0])) {
+        return 0;
+    }
+    let idx = 0;
+    let neg = false;
+    if (digits[0] === '+' || digits[0] === '-') {
+        idx = 1;
+        if (digits[0] === '-') neg = true;
+    }
+    let ans = 0;
+    for (idx; idx < str.length; idx++) {
+        switch (digits.charCodeAt(idx)) {
+            case 48:
+                ans = ans * 10 + 0;
+                break;
+            case 49:
+                ans = ans * 10 + 1;
+                break;
+            case 50:
+                ans = ans * 10 + 2;
+                break;
+            case 51:
+                ans = ans * 10 + 3;
+                break;
+            case 52:
+                ans = ans * 10 + 4;
+                break;
+            case 53:
+                ans = ans * 10 + 5;
+                break;
+            case 54:
+                ans = ans * 10 + 6;
+                break;
+            case 55:
+                ans = ans * 10 + 7;
+                break;
+            case 56:
+                ans = ans * 10 + 8;
+                break;
+            case 57:
+                ans = ans * 10 + 9;
+                break;
+            default:
+                return ans;
+        }
+    }
+    return ans;
+};
