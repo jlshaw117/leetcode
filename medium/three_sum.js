5cd755afaf74106ae0730a82c993a56ad3acce0a
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-28 15:27:51 -0800

    Add brute force solution

--- medium/three_sum.js
+++ medium/three_sum.js
@@ -1,9 +1,9 @@
 const threeSum = (nums) => {
-    let ans = [];
+    let ans = new Set();
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             for (let x = j + 1; x < nums.length; x++) {
-                if (nums[i] + nums[j] + nums[x] === 0) ans.push([nums[i], nums[j], nums[x]]);
+                if (nums[i] + nums[j] + nums[x] === 0) ans.add([nums[i], nums[j], nums[x]].sort((a,b) => a - b));
             }
         }
     }
