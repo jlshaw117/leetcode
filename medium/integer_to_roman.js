2e57c53edab14c00d7ad082edf8edc60f4bdb301
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-28 14:04:28 -0800

    Add solution for integer_to_roman

---
+++ medium/integer_to_roman.js
@@ -0,0 +1,8 @@
+const intToRoman = (num) => {
+    const M = ['', 'M', 'MM', 'MMM'];
+    const C = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+    const X = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+    const I = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[Math.floor(num % 10)];
+};
