f8e4711835db6087498fd3fbbeb531386c3624be
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-30 15:58:36 -0800

    Add different solution

--- easy/valid_parentheses.js
+++ easy/valid_parentheses.js
@@ -1,43 +1,69 @@
-const isValid = (str) => {
-    let stack = [];
-    let open = new Set(['(', '{', '[']);
-    let close = new Set([')', '}', ']']);
+// const isValid = (str) => {
+//     let stack = [];
+//     let open = new Set(['(', '{', '[']);
+//     let close = new Set([')', '}', ']']);
 
+
+//     for (let idx = 0; idx < str.length; idx++) {
+//         if (open.has(str[idx])) {
+//             stack.push(str[idx]);
+//         } else if (stack.length > 0) {
+//             let curr = stack.pop();
+//             switch (curr) {
+//                 case '(':
+//                     if (str[idx] === ')') {
+//                         break;
+//                     } else {
+//                         return false;
+//                     }
+//                 case '{':
+//                     if (str[idx] === '}') {
+//                         break;
+//                     } else {
+//                         return false;
+//                     }
+//                 case '[':
+//                     if (str[idx] === ']') {
+//                         break;
+//                     } else {
+//                         return false;
+//                     }
+//                 default:
+//                     break;
+//             }
+//         } else {
+//             return false;
+//         }
+//     }
+//     if (stack.length === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// Different Approach
+const isValid = (str) => {
+    const combo = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    let stack = [];
 
     for (let idx = 0; idx < str.length; idx++) {
-        if (open.has(str[idx])) {
+        if (combo[str[idx]]) {
             stack.push(str[idx]);
-        } else if (stack.length > 0) {
-            let curr = stack.pop();
-            switch (curr) {
-                case '(':
-                    if (str[idx] === ')') {
-                        break;
-                    } else {
-                        return false;
-                    }
-                case '{':
-                    if (str[idx] === '}') {
-                        break;
-                    } else {
-                        return false;
-                    }
-                case '[':
-                    if (str[idx] === ']') {
-                        break;
-                    } else {
-                        return false;
-                    }
-                default:
-                    break;
-            }
-        } else {
+        } else if (stack.length === 0) {
             return false;
+        } else {
+            let curr = stack.pop();
+            if (str[idx] !== combo[curr]) return false;
         }
     }
-    if (stack.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
+
+    return stack.length === 0;
 };
+
+isValid('(]');
