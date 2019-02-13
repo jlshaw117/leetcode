fdf3fd99e5b0fad832dbbdcc70c792578900d790
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-13 13:12:58 -0800

    Update Brute force

--- hard/shortest_palindrome.js
+++ hard/shortest_palindrome.js
@@ -1,19 +1,25 @@
 const isPalindrome = (str) => {
-    let reversed = str.split('').reverse().join('');
-    return str === reversed;
+    // let reversed = str.split('').reverse().join('');
+    // return str === reversed;
+    for (let idx = 0; idx < str.length / 2; idx++) {
+        if (str[idx] === str[str.length - idx - 1]) {
+            continue;
+        } else {
+            return false
+        }
+    }
+    return true
 };
 
 const shortestPalindrome = (str) => {
+    if (str.length === 0) return str
     let prefix = '';
     for (let idx = str.length; idx > 0; idx--) {
         let sub = str.slice(0, idx);
         if (isPalindrome(sub)) {
-            prefix = str.slice(idx).split('').reverse().join('');
-            break;
+            return str.slice(idx).split('').reverse().join('') + str;
         }
     }
-
-    return prefix + str;
 };
 
 console.log(shortestPalindrome('abcd'));
