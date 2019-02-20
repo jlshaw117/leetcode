fcc3e02144cd6bcd77c964d57fafe062ef17886b
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-20 13:30:55 -0800

    Add solution

---
+++ medium/combination_sum.js
@@ -0,0 +1,28 @@
+const combinationSum = (nums, target) => {
+    if (target === 0) return [];
+    if (nums.length === 0) return [];
+
+    let result = [];
+
+    const rec = (currentResult, newTarget, idx) => {
+        if (newTarget === 0) {
+            result.push(currentResult.slice());
+            return;
+        } else {
+            for (;idx < nums.length; idx++) {
+                if (nums[idx] <= newTarget) {
+                    currentResult.push(nums[idx]);
+                    newTarget -= nums[idx];
+                    rec(currentResult, newTarget, idx);
+                    newTarget += currentResult.pop();
+                    
+                }
+            }
+        }
+    };
+    rec([], target, 0);
+    return result;
+};
+
+console.log(combinationSum([2,3,6,7], 7));
+console.log(combinationSum([2,3,6,7], 7)[1]);
