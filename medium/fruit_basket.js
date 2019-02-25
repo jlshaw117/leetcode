42309f010553e8e69c43315e113f38284215057f
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-25 12:49:10 -0800

    Refactor

--- medium/fruit_basket.js
+++ medium/fruit_basket.js
@@ -6,17 +6,19 @@ const totalFruit = (trees) => {
 
     for (let i = 0; i < trees.length; i++) {
         let newCount = 0;
-        for (let j = i + 1; j < trees.length; j++) {
-            if (baskets.length === 2 && !baskets.has(trees[j])) {
+        for (let j = i; j < trees.length; j++) {
+            if (baskets.size === 2 && !baskets.has(trees[j])) {
                 // if (newCount > count) count = newCount;
                 // baskets = new Set();
                 break;
             }
             baskets.add(trees[j]);
-            count ++;
+            newCount ++;
         }
         if (newCount > count) count = newCount;
         baskets = new Set();
     }
     return count;
 };
+
+console.log(totalFruit([0,1,2,2]));
