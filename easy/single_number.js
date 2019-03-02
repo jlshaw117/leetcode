675428250344dda2fad71d07cbd423ca5ca12331
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-01 23:33:44 -0800

    Add optimized

--- easy/single_number.js
+++ easy/single_number.js
@@ -1,10 +1,16 @@
+// const singleNumber = (nums) => {
+//     let set = new Set(nums);
+
+//     let sum = 0;
+//     set.forEach(num => sum += num);
+
+//     return (2 * sum) - nums.reduce((agg, num) => agg + num); 
+// };
+
+// Optomized
+
 const singleNumber = (nums) => {
-    let set = new Set(nums);
-
-    let sum = 0;
-    set.forEach(num => sum += num);
-
-    return (2 * sum) - nums.reduce((agg, num) => agg + num); 
+    return nums.reduce((agg, num) => agg ^ num);
 };
 
 console.log(singleNumber([2,2,1]));
