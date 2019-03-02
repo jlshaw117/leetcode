c5464c34c99bf447e681cd2be717ed32d41a81fd
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-01 23:31:34 -0800

    Add brute force solution

---
+++ easy/single_number.js
@@ -0,0 +1,10 @@
+const singleNumber = (nums) => {
+    let set = new Set(nums);
+
+    let sum = 0;
+    set.forEach(num => sum += num);
+
+    return (2 * sum) - nums.reduce((agg, num) => agg + num); 
+};
+
+console.log(singleNumber([2,2,1]));
