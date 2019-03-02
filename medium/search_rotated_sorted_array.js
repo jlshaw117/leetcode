fa03193f892a88872dfc9b6346fb5e7da026176d
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-01 20:39:47 -0800

    Add iterative solution

--- medium/search_rotated_sorted_array.js
+++ medium/search_rotated_sorted_array.js
@@ -36,3 +36,31 @@
 //         }
 //     }
 // };
+
+// Itterative
+
+const search = (nums, target) => {
+    if (nums.length === 0) return -1;
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) return mid;
+
+        if (nums[start] < nums[mid]) {
+            if (target < nums[mid] && target >= nums[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return target === nums[start] ? start : -1;
+};
