73d1c2b061101c14855360433d7b48bb15775e7f
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-24 14:11:30 -0700

    Add optimized

--- easy/merge_two_binary_trees.js
+++ easy/merge_two_binary_trees.js
@@ -1,10 +1,21 @@
+// const mergeTrees = (tree1, tree2) => {
+//     if (!tree1) return tree2;
+//     if (!tree2) return tree1;
+//     let newNode = new TreeNode(tree1.val + tree2.val);
+
+//     newNode.left = mergeTrees(tree1.left, tree2.left);
+//     newNode.right = mergeTrees(tree1.right, tree2.right);
+    
+//     return newNode;
+// };
+// Slight Optimization for space
 const mergeTrees = (tree1, tree2) => {
     if (!tree1) return tree2;
     if (!tree2) return tree1;
-    let newNode = new TreeNode(tree1.val + tree2.val);
+    tree1.val = tree1.val + tree2.val;
 
-    newNode.left = mergeTrees(tree1.left, tree2.left);
-    newNode.right = mergeTrees(tree1.right, tree2.right);
-    
-    return newNode;
+    tree1.left = mergeTrees(tree1.left, tree2.left);
+    tree1.right = mergeTrees(tree1.right, tree2.right);
+
+    return tree1;
 };
