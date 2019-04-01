8b168e39bec5ef3a231362a66c2d664d48d62ef7
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-31 21:05:51 -0700

    Add optimized

--- easy/fib_number.js
+++ easy/fib_number.js
@@ -1,4 +1,17 @@
+// Brute Force
+// const fib = (n) => {
+//     if (n < 2) return n;
+//     return fib(n -1) + fib(n - 2);
+// };
+
+// Optimized
+
 const fib = (n) => {
-    if (n < 2) return n;
-    return fib(n -1) + fib(n - 2);
+    let fibs = [0,1];
+    while (fibs.length < n + 1) {
+        let i = fibs.length - 1;
+        let j = fibs.length - 2;
+        fibs.push(fibs[i] + fibs[j]);
+    }
+    return fibs[n];
 };
