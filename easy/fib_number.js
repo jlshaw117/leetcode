879c12e57ce6c67e20c4fc6d68216dc5fbbd4e52
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-31 21:14:08 -0700

    Add space optimization

--- easy/fib_number.js
+++ easy/fib_number.js
@@ -6,12 +6,27 @@
 
 // Optimized
 
+// const fib = (n) => {
+//     let fibs = [0,1];
+//     while (fibs.length < n + 1) {
+//         let i = fibs.length - 1;
+//         let j = fibs.length - 2;
+//         fibs.push(fibs[i] + fibs[j]);
+//     }
+//     return fibs[n];
+// };
+
+// Optimized Space
+
 const fib = (n) => {
-    let fibs = [0,1];
-    while (fibs.length < n + 1) {
-        let i = fibs.length - 1;
-        let j = fibs.length - 2;
-        fibs.push(fibs[i] + fibs[j]);
+    if (n < 2) return n;
+    let pre = 0;
+    let curr = 1;
+    let result;
+    for (let i = 2; i <= n; i++) {
+        result = pre + curr;
+        pre = curr;
+        curr = result;
     }
-    return fibs[n];
+    return result;
 };
