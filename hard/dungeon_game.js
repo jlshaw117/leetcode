9c1c140184c395d584b2514509b0904efc18d5d9
Jerrik Shaw <jlshaw117@gmail.com>
2019-05-08 12:56:20 -0700

    Add optimized solution

--- hard/dungeon_game.js
+++ hard/dungeon_game.js
@@ -32,24 +32,24 @@
 //     memo[key] = reqHealth <= 0 ? 1 : reqHealth;
 //     return memo[key];
 // };
-
-// const calculateMinimumHP = (dungeon) => {
-//     lastx = dungeon.length - 1;
-//     lasty = dungeon[0].length - 1;
-//     lastVal = dungeon[lastx][lasty];
-//     dungeon[lastx][lasty] = lastVal >= 0 ? 1 : Math.abs(dungeon[lastx][lasty]) + 1;
-//     for (let x = dungeon.length - 1; x >= 0; x--) {
-//         for (let y = dungeon[0].length - 1; y >= 0; y--) {
-//             if (x === lastx && y === lasty) continue;
-//             let right = y >= lasty ? Infinity : dungeon[x][y + 1];
-//             let down = x >= lastx ? Infinity : dungeon[x + 1][y];
-//             let remHealth = Math.min(right, down);
-//             remHealth -= dungeon[x][y];
-//             remHealth = remHealth <= 0 ? 1 : remHealth;
-//             dungeon[x][y] = remHealth;
-//         }
-//     }
-//     return dungeon[0][0];
-// };
+// Optimized
+const calculateMinimumHP = (dungeon) => {
+    lastx = dungeon.length - 1;
+    lasty = dungeon[0].length - 1;
+    lastVal = dungeon[lastx][lasty];
+    dungeon[lastx][lasty] = lastVal >= 0 ? 1 : Math.abs(dungeon[lastx][lasty]) + 1;
+    for (let x = dungeon.length - 1; x >= 0; x--) {
+        for (let y = dungeon[0].length - 1; y >= 0; y--) {
+            if (x === lastx && y === lasty) continue;
+            let right = y >= lasty ? Infinity : dungeon[x][y + 1];
+            let down = x >= lastx ? Infinity : dungeon[x + 1][y];
+            let remHealth = Math.min(right, down);
+            remHealth -= dungeon[x][y];
+            remHealth = remHealth <= 0 ? 1 : remHealth;
+            dungeon[x][y] = remHealth;
+        }
+    }
+    return dungeon[0][0];
+};
 
 console.log(calculateMinimumHP([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]]));
