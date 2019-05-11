bb0cf25fe107f9b9e3b8de497d1eb377d63f7f8e
Jerrik Shaw <jlshaw117@gmail.com>
2019-05-10 18:36:31 -0700

    Add iterative solution

--- medium/binary_tree_in_order.js
+++ medium/binary_tree_in_order.js
@@ -4,3 +4,48 @@
 //     return inorderTraversal(root.left).concat([root.val], inorderTraversal(root.right));
 // };
 
+// Iterative Solution
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const inorderTraversal = (root) => {
+    let stack = [];
+    let node = root;
+    let result = [];
+    if (node.left) {
+        while(node) {
+            stack.push(node);
+            node = node.left;
+        } 
+    } else {
+        stack.push(node);
+    }
+    
+
+    while (stack.length) {
+        let curr = stack.pop();
+        result.push(curr.val);
+        if (curr.right) {
+            node = curr.right;
+            while (node) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+    }
+    return result;
+};
+
+let root = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+root.left = node2;
+// node2.left = node3;
+
+console.log(inorderTraversal(root));
