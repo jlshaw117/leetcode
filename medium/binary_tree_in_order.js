e33d6e171b8038a396222ae874339d609ea61dae
Jerrik Shaw <jlshaw117@gmail.com>
2019-05-10 18:20:58 -0700

    Add recursive solution

---
+++ medium/binary_tree_in_order.js
@@ -0,0 +1,6 @@
+// Recursive Solution
+// const inorderTraversal = (root) => {
+//     if (!root) return [];
+//     return inorderTraversal(root.left).concat([root.val], inorderTraversal(root.right));
+// };
+
