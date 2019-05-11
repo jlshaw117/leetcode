5389cf77a4059c0c1336114e957c708b2fb2be58
Jerrik Shaw <jlshaw117@gmail.com>
2019-05-10 20:28:46 -0700

    Add brute force solution

--- medium/three_sum.js
+++ medium/three_sum.js
@@ -1,11 +1,23 @@
 const threeSum = (nums) => {
-    let ans = new Set();
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let x = j + 1; x < nums.length; x++) {
-                if (nums[i] + nums[j] + nums[x] === 0) ans.add([nums[i], nums[j], nums[x]].sort((a,b) => a - b));
+    let result = [];
+    let set = new Set();
+    if (nums.length < 3) return result;
+    let sorted = nums.sort((a, b) => a - b);
+    for (let x = 0; x < sorted.length - 2; x++) {
+        let a = sorted[x];
+        for (let y = x + 1; y < sorted.length - 1; y++) {
+            let b = sorted[y];
+            for (let z = y + 1; z < sorted.length; z++) {
+                let c = sorted[z];
+                let key = `${a},${b},${c}`;
+                if (a+b+c === 0 && !set.has(key)) {
+                    result.push([a,b,c]);
+                    set.add(key);
+                }
             }
         }
     }
-    return ans;
+    return result;
 };
+
+console.log(...threeSum([-1, 0, 1, 2, -1, -4]));
