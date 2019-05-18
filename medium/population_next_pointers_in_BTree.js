bb90792f1ed7202704f164424356cb310c7569ec
Jerrik Shaw <jlshaw117@gmail.com>
2019-05-18 11:26:48 -0700

    Add recursive solution

--- medium/population_next_pointers_in_BTree.js
+++ medium/population_next_pointers_in_BTree.js
@@ -1,15 +1,28 @@
-var connect = function (root) {
-    if (!root) return root;
-    let queue = [root];
-    while (queue.length) {
-        let levelCount = queue.length;
-        for (let i = 0; i < levelCount; i++) {
-            let currNode = queue.shift();
-            if (currNode.left) queue.push(currNode.left);
-            if (currNode.right) queue.push(currNode.right);
-            if (i === levelCount - 1) continue;
-            currNode.next = queue[0];
-        }
-    }
+// var connect = function (root) {
+//     if (!root) return root;
+//     let queue = [root];
+//     while (queue.length) {
+//         let levelCount = queue.length;
+//         for (let i = 0; i < levelCount; i++) {
+//             let currNode = queue.shift();
+//             if (currNode.left) queue.push(currNode.left);
+//             if (currNode.right) queue.push(currNode.right);
+//             if (i === levelCount - 1) continue;
+//             currNode.next = queue[0];
+//         }
+//     }
+//     return root;
+// };
+// recursive
+
+const connect = (root) => {
+    if (!root || root.left) return root;
+
+    root.left.next = root.right;
+    root.right.next = root.next ? root.next.left : null;
+
+    connect(root.left);
+    connect(root.right);
+
     return root;
 };
